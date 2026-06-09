178f5b6a719d584cd9fad7db5f19533a9c1d29f9
zxj <3189323080@qq.com>
2026-06-09 22:55:24 +0800

    feat: 添加基础路径配置以支持项目部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://zgydYL.github.io',
+	base: '/Astro-Blog/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
